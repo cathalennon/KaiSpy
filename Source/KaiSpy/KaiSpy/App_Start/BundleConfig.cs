3a475aa2fb85ddcc81d6e24cce264d73a879a2fc
Ashley <ashley.j.stewart@live.com>
2015-04-24 11:53:56 +1200

    WeMadeItWork

--- Source/KaiSpy/KaiSpy/App_Start/BundleConfig.cs
+++ Source/KaiSpy/KaiSpy/App_Start/BundleConfig.cs
@@ -20,9 +20,11 @@ namespace KaiSpy
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+         bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/KaiSpyContent.css"));
+
+             bundles.Add(new ScriptBundle("~/bundles/App").Include(
+                      "~/Scripts/KaiSpyApp.js"));
         }
     }
 }

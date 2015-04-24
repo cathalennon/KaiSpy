f301555073847e8e7eba18e335357614c643d14b
nary-dev <nary_silver@hotmail.com>
2015-04-24 15:49:50 +1200

    addMarker function working and shows resturant name on click

--- Source/KaiSpy/KaiSpy/Scripts/KaiSpyApp.js
+++ Source/KaiSpy/KaiSpy/Scripts/KaiSpyApp.js
@@ -1,51 +1,77 @@
 ﻿$('document').ready(function() {
-    var map;
-
-    function initialize() {
-        var mapOptions = {
-            zoom: 18
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
-        // Try HTML5 geolocation
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var pos = new google.maps.LatLng(position.coords.latitude,
-                                                 position.coords.longitude);
-
-                var infowindow = new google.maps.Marker({
-                    map: map,
-                    position: pos,
-                    title: 'You Hungry nom nom nom'
-                });
-
-                map.setCenter(pos);
-            }, function () {
-                handleNoGeolocation(true);
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleNoGeolocation(false);
-        }
-    }
-
-    function handleNoGeolocation(errorFlag) {
-        if (errorFlag) {
-            var content = 'Error: The Geolocation service failed.';
-        } else {
-            var content = 'Error: Your browser doesn\'t support geolocation.';
-        }
-
-        var options = {
-            map: map,
-            position: new google.maps.LatLng(60, 105),
-            content: content
-        };
-
-        var infowindow = new google.maps.InfoWindow(options);
-        map.setCenter(options.position);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
+    initialize();
+    addMarker(ekim);
 });
+
+var map;
+var ekim = {
+    name: "Ekim Burger",
+    lattitude: -41.296109,
+    longitude: 174.774226
+}; 
+
+function initialize() {
+    var mapOptions = {
+        zoom: 18
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                                             position.coords.longitude);
+
+            var infowindow = new google.maps.Marker({
+                map: map,
+                position: pos,
+                title: 'You Hungry nom nom nom'
+            });
+
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+}
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+function addMarker(resturant) {
+    var name = resturant.name;
+    var LatLong = new google.maps.LatLng(resturant.lattitude, resturant.longitude);
+    var infowindow = new google.maps.InfoWindow({
+        content: '<div class="marker">'+'<h3>'+ resturant.name +'</h3>'+ '</div>'
+    });
+
+    var marker = new google.maps.Marker({
+        position: LatLong,
+        map: map,
+        title: name
+    });
+    google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+

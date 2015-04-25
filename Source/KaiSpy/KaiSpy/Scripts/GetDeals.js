9ee55c882fa10d717f8538d07540b89546ca88a4
nary-dev <nary_silver@hotmail.com>
2015-04-25 14:02:41 +1200

    addedGetDeal class with getAllDeals function

---
+++ Source/KaiSpy/KaiSpy/Scripts/GetDeals.js
@@ -0,0 +1,14 @@
+﻿var ApiRequest = function() {
+    this.uri = 'http://localhost:59080/api';
+}
+
+ApiRequest.prototype.getAllDeals = function() {
+    $.ajax({
+        type: 'GET',
+        url: this.uri + '',
+    }).done(function(data) {
+        return data;
+    }).fail(function(error) {
+        console.log('cannot get deals from server. Error: ' + error);
+    });
+}

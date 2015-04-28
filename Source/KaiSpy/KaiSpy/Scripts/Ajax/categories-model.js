b373c627effa1c1c667a6248fb20113cec1022be
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-04-28 17:30:25 +1200

    oops added to dev

---
+++ Source/KaiSpy/KaiSpy/Scripts/Ajax/categories-model.js
@@ -0,0 +1,49 @@
+﻿function AjaxCategoriesModel() {
+
+    this.BaseURI = "http://localhost:59080/api/categories";
+    this.View = new CategoriesView();
+
+}
+
+AjaxCategoriesModel.prototype.GetAllCategories = function () {
+    view = new CategoriesView();
+    $.ajax({
+        type: "GET",
+        url: this.BaseURI,
+        datatype: "Json",
+        success: function (response) {
+            view.CreateCategoryCheckBoxes(response);
+        },
+        error: function (response) {
+            alert(response);
+        }
+    });
+}
+
+AjaxCategoriesModel.prototype.GetDealsFromCategoryCheckbox = function (keyword) {
+    $.ajax({
+        type: "GET",
+        url: this.BaseURI + '/' + keyword,
+        datatype: "json",
+        success: function (response) {
+            LoopThroughJSON(response.Deals);
+        },
+        error: function (response) {
+            alert(response);
+        }
+    });
+}
+
+AjaxCategoriesModel.prototype.GetDealsFromCategoryUnCheckbox = function (keyword) {
+    $.ajax({
+        type: "GET",
+        url: this.BaseURI + "/" + keyword,
+        datatype: "json",
+        success: function (response) {
+            removeMarkers(response.Deals);
+        },
+        error: function (response) {
+            alert(response);
+        }
+    });
+}

302a66ca0e41c01912536c416221d06ad5744b47
Ashley <ashley.j.stewart@live.com>
2015-04-29 21:22:38 +1200

    Ash Changes

--- Source/KaiSpy/KaiSpy.Tests/Scripts/Radius and Slider/DispayRadius_spec.js
+++ Source/KaiSpy/KaiSpy.Tests/Scripts/Radius and Slider/DispayRadius_spec.js
@@ -1,13 +1,25 @@
-﻿///<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-/// <reference path="~../../../KaiSpy/Scripts/KaiSpyApp.js"/>
+﻿///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"/>
+///<reference path="~../../../KaiSpy/Scripts/KaiSpyApp.js"/>
 
 
 describe("Display Circle Radius", function () {
-    it("Displays Radius in KM", function () {
-        var value = 4;
-        DisplaySearchRadiusOnPage(value);
-        var test = document.getElementById('search-radius').text();
-        expect(test).toEqual(4 + "km");
+    it("to equal 4km.", function (){
+        var FakerElement = jasmine.createSpyObj('element', ["text"])
+        $ = function() {return(FakerElement);}
+
+        DisplaySearchRadiusOnPage(4);
+        expect(FakerElement.text).toHaveBeenCalledWith('4 km');
      });
-});
+}); 
+
+
+
+//describe("SetCircleRadiusBySlider", function() {
+//    it("Calls Methods and sets radius size", function() {
+//        expect(()) ? ()
+//
+//    });
+//});
+
+
 

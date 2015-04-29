cca87bf372985416992622595b910dc18f408bec
Ashley <ashley.j.stewart@live.com>
2015-04-29 15:25:37 +1200

    Setup for JS Testing

---
+++ Source/KaiSpy/KaiSpy.Tests/Scripts/Radius and Slider/DispayRadius_spec.js
@@ -0,0 +1,13 @@
+﻿///<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+/// <reference path="~../../../KaiSpy/Scripts/KaiSpyApp.js"/>
+
+
+describe("Display Circle Radius", function () {
+    it("Displays Radius in KM", function () {
+        var value = 4;
+        DisplaySearchRadiusOnPage(value);
+        var test = document.getElementById('search-radius').text();
+        expect(test).toEqual(4 + "km");
+     });
+});
+
